17ecb88f9f61b231a1e4fd72fa393fe4f20eef83
delian1986 <delian1914@gmail.com>
2018-01-15 00:55:27 +0200

    Added Server Logos to Radio Buttons

--- WoWLauncher/WoWLauncher/Form1.Designer.cs
+++ WoWLauncher/WoWLauncher/Form1.Designer.cs
@@ -28,18 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.startButton = new System.Windows.Forms.Button();
             this.warmane = new System.Windows.Forms.RadioButton();
             this.gamerDistrict = new System.Windows.Forms.RadioButton();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // startButton
             // 
-            this.startButton.Location = new System.Drawing.Point(227, 329);
+            this.startButton.Location = new System.Drawing.Point(251, 390);
             this.startButton.Name = "startButton";
             this.startButton.Size = new System.Drawing.Size(75, 23);
             this.startButton.TabIndex = 0;
@@ -50,33 +49,39 @@
             // warmane
             // 
             this.warmane.AutoSize = true;
+            this.warmane.BackColor = System.Drawing.Color.Transparent;
             this.warmane.Checked = true;
             this.warmane.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.warmane.Location = new System.Drawing.Point(26, 222);
+            this.warmane.Image = ((System.Drawing.Image)(resources.GetObject("warmane.Image")));
+            this.warmane.Location = new System.Drawing.Point(12, 227);
             this.warmane.Name = "warmane";
-            this.warmane.Size = new System.Drawing.Size(71, 17);
+            this.warmane.Size = new System.Drawing.Size(142, 145);
             this.warmane.TabIndex = 1;
             this.warmane.TabStop = true;
             this.warmane.Text = "Warmane";
-            this.warmane.UseVisualStyleBackColor = true;
+            this.warmane.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.warmane.UseVisualStyleBackColor = false;
             this.warmane.CheckedChanged += new System.EventHandler(this.Warmane_CheckedChanged);
             // 
             // gamerDistrict
             // 
             this.gamerDistrict.AutoSize = true;
+            this.gamerDistrict.BackColor = System.Drawing.Color.Transparent;
             this.gamerDistrict.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.gamerDistrict.Location = new System.Drawing.Point(26, 282);
+            this.gamerDistrict.Image = ((System.Drawing.Image)(resources.GetObject("gamerDistrict.Image")));
+            this.gamerDistrict.Location = new System.Drawing.Point(428, 227);
             this.gamerDistrict.Name = "gamerDistrict";
-            this.gamerDistrict.Size = new System.Drawing.Size(88, 17);
+            this.gamerDistrict.Size = new System.Drawing.Size(142, 145);
             this.gamerDistrict.TabIndex = 2;
             this.gamerDistrict.Text = "GamerDistrict";
-            this.gamerDistrict.UseVisualStyleBackColor = true;
+            this.gamerDistrict.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.gamerDistrict.UseVisualStyleBackColor = false;
             this.gamerDistrict.CheckedChanged += new System.EventHandler(this.GamerDistrict_CheckedChanged);
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox1.BackgroundImage = global::WoWLauncher.Properties.Resources.WrathLogo;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.pictureBox1.Location = new System.Drawing.Point(12, 38);
             this.pictureBox1.Name = "pictureBox1";
@@ -84,33 +89,23 @@
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
             // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Location = new System.Drawing.Point(400, 38);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
-            this.pictureBox2.TabIndex = 4;
-            this.pictureBox2.TabStop = false;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Silver;
-            this.BackgroundImage = global::WoWLauncher.Properties.Resources._1522548_lich_king;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(554, 406);
-            this.Controls.Add(this.pictureBox2);
+            this.ClientSize = new System.Drawing.Size(582, 450);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.gamerDistrict);
             this.Controls.Add(this.warmane);
             this.Controls.Add(this.startButton);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SimpleWoWLauncher";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -122,7 +117,6 @@
         private System.Windows.Forms.RadioButton warmane;
         private System.Windows.Forms.RadioButton gamerDistrict;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
--- WoWLauncher/WoWLauncher/Properties/Resources.Designer.cs
+++ WoWLauncher/WoWLauncher/Properties/Resources.Designer.cs
@@ -73,9 +73,49 @@ namespace WoWLauncher.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap gd_logo {
+            get {
+                object obj = ResourceManager.GetObject("gd_logo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap warmane {
+            get {
+                object obj = ResourceManager.GetObject("warmane", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap warmane1 {
+            get {
+                object obj = ResourceManager.GetObject("warmane1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap WrathLogo {
             get {
                 object obj = ResourceManager.GetObject("WrathLogo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap WrathLogo1 {
+            get {
+                object obj = ResourceManager.GetObject("WrathLogo1", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
