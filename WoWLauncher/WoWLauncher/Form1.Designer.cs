7e07b5c75360ab835227b6de0404f2d1a8a37aeb
delian1986 <delian1914@gmail.com>
2018-01-14 23:49:05 +0200

    WoWLauncher

---
+++ WoWLauncher/WoWLauncher/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WoWLauncher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.warmane = new System.Windows.Forms.RadioButton();
+            this.gamerDistrict = new System.Windows.Forms.RadioButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(227, 329);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // warmane
+            // 
+            this.warmane.AutoSize = true;
+            this.warmane.Checked = true;
+            this.warmane.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.warmane.Location = new System.Drawing.Point(26, 222);
+            this.warmane.Name = "warmane";
+            this.warmane.Size = new System.Drawing.Size(71, 17);
+            this.warmane.TabIndex = 1;
+            this.warmane.TabStop = true;
+            this.warmane.Text = "Warmane";
+            this.warmane.UseVisualStyleBackColor = true;
+            this.warmane.CheckedChanged += new System.EventHandler(this.Warmane_CheckedChanged);
+            // 
+            // gamerDistrict
+            // 
+            this.gamerDistrict.AutoSize = true;
+            this.gamerDistrict.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.gamerDistrict.Location = new System.Drawing.Point(26, 282);
+            this.gamerDistrict.Name = "gamerDistrict";
+            this.gamerDistrict.Size = new System.Drawing.Size(88, 17);
+            this.gamerDistrict.TabIndex = 2;
+            this.gamerDistrict.Text = "GamerDistrict";
+            this.gamerDistrict.UseVisualStyleBackColor = true;
+            this.gamerDistrict.CheckedChanged += new System.EventHandler(this.GamerDistrict_CheckedChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::WoWLauncher.Properties.Resources.WrathLogo;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(168, 90);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(400, 38);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.BackgroundImage = global::WoWLauncher.Properties.Resources._1522548_lich_king;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(554, 406);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.gamerDistrict);
+            this.Controls.Add(this.warmane);
+            this.Controls.Add(this.startButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SimpleWoWLauncher";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.RadioButton warmane;
+        private System.Windows.Forms.RadioButton gamerDistrict;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ WoWLauncher/WoWLauncher/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace WoWLauncher
+{
+    public partial class Form1 : Form
+    {
+        string realmList= "set realmlist logon.warmane.com";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Warmane_CheckedChanged(object sender, EventArgs e)
+        {
+            if (((RadioButton)sender).Checked)
+            {
+                realmList = "set realmlist logon.warmane.com";
+
+            }
+        }
+
+        private void GamerDistrict_CheckedChanged(object sender, EventArgs e)
+        {
+            if (((RadioButton)sender).Checked)
+            {
+
+                realmList = "set realmlist wow.gamer-district.org";
+            }
+        }
+
+
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(@"C:\World of Warcraft Wrath of the Lich King 3.3.5 enUS\Data\enUS\realmlist.wtf", realmList);
+
+            Process startGame = new Process();
+            startGame.StartInfo.FileName = "C:\\World of Warcraft Wrath of the Lich King 3.3.5 enUS\\Wow.exe";
+            startGame.Start();
+        }
+    }
+}
