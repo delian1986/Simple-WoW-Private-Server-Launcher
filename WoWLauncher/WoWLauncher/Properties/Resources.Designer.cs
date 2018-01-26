c2e37e997d5488901f215b7ef0af8b75abb6f9d0
delian1986 <delian1914@gmail.com>
2018-01-26 23:58:15 +0200

    Background Fixed

--- WoWLauncher/WoWLauncher/Properties/Resources.Designer.cs
+++ WoWLauncher/WoWLauncher/Properties/Resources.Designer.cs
@@ -83,16 +83,6 @@ namespace WoWLauncher.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap warmane {
-            get {
-                object obj = ResourceManager.GetObject("warmane", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap warmane1 {
             get {
                 object obj = ResourceManager.GetObject("warmane1", resourceCulture);
